5106770c15b9639ee2dfd21e7f788b7792f737c8
Noe-akili <Noe-akili@users.noreply.github.com>
2026-08-19 20:51:08 +0200

    feat: ajout exe.js, extra.js, tools.js + refactor modules lib/

--- index.js
+++ index.js
@@ -4,6 +4,8 @@
 // CLI complète (commander/chalk) chargée uniquement pour les autres commandes.
 
 const _args = process.argv.slice(2);
+const _flags = [];
+while (_args[0] === '--offline') { _flags.push(_args.shift()); }
 if (_args[0] === 'run' && !_args.some(a => ['-h', '--help', '-V', '--version'].includes(a))) {
   const { fastRun } = await import('./lib/fastrun.js');
   process.exit(await fastRun(_args.slice(1)));
